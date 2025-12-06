a2e4e55ff5b6b5f6194369ccc655206986c4b6e4
dattu145 <sridharreddy145233@gmail.com>
2025-12-06 18:27:43 +0530

    Modified config

--- app.py
+++ app.py
@@ -1,6 +1,30 @@
-from app import create_app
+from flask import Flask
+from config import Config
+from supabase import create_client
+import os
 
-app = create_app()
+app = Flask(__name__)
+app.config.from_object(Config)
 
-if __name__ == '__main__':
+# -------------------------------
+# Supabase Init
+# -------------------------------
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
+
+# Make supabase available to blueprints
+from app import auth, routes
+
+# Register blueprints
+app.register_blueprint(auth.auth_bp)
+app.register_blueprint(routes.main_bp)
+
+# Home route
+@app.route("/")
+def home():
+    return "Hello Render"
+
+
+if __name__ == "__main__":
     app.run(debug=True)
